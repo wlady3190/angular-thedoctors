5e57829ad2c0041ad27f265e76c475ce2f5ef4d3
wlady3190 <wlady_3190@hotmail.com>
2023-05-01 19:47:15 -0500

    Quit unfuctional links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { E404Component } from './components/e404/e404.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { FormCitaComponent } from './components/form-cita/form-cita.component';
+import { MapComponent } from './components/map/map.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'servicios', component: CarrouselComponent },
   { path: 'app-cita', component: FormCitaComponent },
-
-
+  { path: 'contacto', component: MapComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: E404Component },
 ];
